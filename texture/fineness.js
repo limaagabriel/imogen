96cae4f5fe099c63a35f47c00800830f86eac5f9
Gabriel Alves <itsmealves@gmail.com>
2018-05-08 14:08:29 -0300

    fineness corrigida

--- texture/fineness.js
+++ texture/fineness.js
@@ -4,16 +4,18 @@ function fineness() {
         run: function(image, features) {
             let a = 0;
             let b = 0;
+            let c = 0;
             const ngtdm = features.ngtdm;
 
             for(let i = 0; i < ngtdm.length; i++) {
                 const iRow = ngtdm[i];
                 a += iRow.p * iRow.s;
+                c += i * iRow.p;
+            }
 
-                for(let j = 0; j < ngtdm.length; j++) {
-                    const jRow = ngtdm[j];
-                    b += (i * iRow.p) - (j * jRow.p);
-                }
+            for(let j = 0; j < ngtdm.length; j++) {
+                const jRow = ngtdm[j];
+                b += c - (j * jRow.p);
             }
 
             return a / b;
