b29f722b785979c83e5566be255962a0341a539c
Gabriel Alves <itsmealves@gmail.com>
2019-01-03 20:43:00 -0300

    images

--- featureExtraction.js
+++ featureExtraction.js
@@ -25,58 +25,61 @@ const centerDistanceSignature = require('./extraction/centerDistanceSignature');
 const ProcessingPipe = require('./processing/pipe');
 const FeatureExtractionPipe = require('./extraction/pipe');
 
-const imageName = 'image4.bmp';
+const imageName = 'old.jpg';
 const imagePath = path.join('resources', imageName);
 
 const main = image => {
     const regionProcessingPipe = new ProcessingPipe(image);
-    const contourProcessingPipe = new ProcessingPipe(image);
-    const contour = contourProcessingPipe.apply(binarize())
-                                         .apply(erosion())
-                                         .apply(subtractFrom(image))
-                                         .apply(binarize())
-                                         .collect();
+
+    // const contourProcessingPipe = new ProcessingPipe(image);
+    // const contour = contourProcessingPipe.apply(binarize())
+    //                                      .apply(erosion())
+    //                                      .apply(subtractFrom(image))
+    //                                      .apply(binarize())
+    //                                      .collect();
 
     const region = regionProcessingPipe.apply(binarize())
+                                       .apply(negative())
+                                       .apply(removeAlphaFrom(255))
                                        .collect();
 
-    const contourExtractionPipe = new FeatureExtractionPipe(contour, {});
-    const contourFeatures = contourExtractionPipe.apply(pointSet())
-                                                 .apply(contourArea())
-                                                 .apply(contourMassCenter())
-                                                 .apply(centerDistanceSignature())
-                                                 .apply(complexCoordinates())
-                                                 .apply(fourierDescriptors())
-                                                 .collect();
+    // const contourExtractionPipe = new FeatureExtractionPipe(contour, {});
+    // const contourFeatures = contourExtractionPipe.apply(pointSet())
+    //                                              .apply(contourArea())
+    //                                              .apply(contourMassCenter())
+    //                                              .apply(centerDistanceSignature())
+    //                                              .apply(complexCoordinates())
+    //                                              .apply(fourierDescriptors())
+    //                                              .collect();
 
-    const regionExtractionPipe = new FeatureExtractionPipe(region, {});
-    const regionFeatures = regionExtractionPipe.apply(hu())
-                                               .apply(pointSet())
-                                               .apply(grahamConvexHull())
-                                               .apply(convexHullContour())
-                                               .apply(convexDeficiency())
-                                               .apply(minBoundingBox())
-                                               .apply(aspectRatio())
-                                               .apply(convexity())
-                                               .collect();
+    // const regionExtractionPipe = new FeatureExtractionPipe(region, {});
+    // const regionFeatures = regionExtractionPipe.apply(hu())
+    //                                            .apply(pointSet())
+    //                                            .apply(grahamConvexHull())
+    //                                            .apply(convexHullContour())
+    //                                            .apply(convexDeficiency())
+    //                                            .apply(minBoundingBox())
+    //                                            .apply(aspectRatio())
+    //                                            .apply(convexity())
+    //                                            .collect();
 
-    fs.writeFile(path.join('out', 'regionFeatures.json'), JSON.stringify(regionFeatures, null, '\t'), err => {
-        if(err) console.log('Error writing to regionFeatures.json');
-        else console.log('regionFeatures.json ready!');
+    // fs.writeFile(path.join('out', 'regionFeatures.json'), JSON.stringify(regionFeatures, null, '\t'), err => {
+    //     if(err) console.log('Error writing to regionFeatures.json');
+    //     else console.log('regionFeatures.json ready!');
+    // });
+
+    // fs.writeFile(path.join('out', 'contourFeatures.json'), JSON.stringify(contourFeatures, null, '\t'), err => {
+    //     if(err) console.log('Error writing to contourFeatures.json');
+    //     else console.log('contourFeatures.json ready!');
+    // });
+
+    region.write(path.join('out', 'regionbig.png'), () => {
+        console.log(path.join('out', 'regionbig.png'), 'ready!')
     });
 
-    fs.writeFile(path.join('out', 'contourFeatures.json'), JSON.stringify(contourFeatures, null, '\t'), err => {
-        if(err) console.log('Error writing to contourFeatures.json');
-        else console.log('contourFeatures.json ready!');
-    });
-
-    region.write(path.join('out', 'region' + imageName), () => {
-        console.log(path.join('out', 'region' + imageName), 'ready!')
-    });
-
-    contour.write(path.join('out', 'contour' + imageName), () => {
-        console.log(path.join('out', 'contour' + imageName), 'ready!')
-    });
+    // contour.write(path.join('out', 'contour' + imageName), () => {
+    //     console.log(path.join('out', 'contour' + imageName), 'ready!')
+    // });
 };
 
 const readError = console.log;
